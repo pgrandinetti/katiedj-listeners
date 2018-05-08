6bb2ceef14d624e57db593166224b0407418f6ac
pietro <pietro.gra@hotmail.it>
2018-05-08 18:07:29 +0200

    Listening to remote server IP

--- python/params.py
+++ python/params.py
@@ -1,5 +1,5 @@
-# address = '172.104.155.87'
-address = '127.0.0.1'
+address = '172.104.155.87'
+# address = '127.0.0.1'
 port = '8080'
 endpoint = '/macro/sample/'
 URL = 'ws://' + address + ':' + port + endpoint
